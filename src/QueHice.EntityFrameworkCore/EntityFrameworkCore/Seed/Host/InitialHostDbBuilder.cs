dd0ec07d5fe95ae442bdfaa187aa653926885b6f
Giorgio <misuperbuzon@hotmail.com>
2020-05-14 21:14:45 -0300

    Add files via upload

---
+++ src/QueHice.EntityFrameworkCore/EntityFrameworkCore/Seed/Host/InitialHostDbBuilder.cs
@@ -0,0 +1,22 @@
+﻿namespace QueHice.EntityFrameworkCore.Seed.Host
+{
+    public class InitialHostDbBuilder
+    {
+        private readonly QueHiceDbContext _context;
+
+        public InitialHostDbBuilder(QueHiceDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Create()
+        {
+            new DefaultEditionCreator(_context).Create();
+            new DefaultLanguagesCreator(_context).Create();
+            new HostRoleAndUserCreator(_context).Create();
+            new DefaultSettingsCreator(_context).Create();
+
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ src/QueHice.EntityFrameworkCore/EntityFrameworkCore/Seed/Tenants/DefaultTenantBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Abp.MultiTenancy;
+using QueHice.Editions;
+using QueHice.MultiTenancy;
+
+namespace QueHice.EntityFrameworkCore.Seed.Tenants
+{
+    public class DefaultTenantBuilder
+    {
+        private readonly QueHiceDbContext _context;
+
+        public DefaultTenantBuilder(QueHiceDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Create()
+        {
+            CreateDefaultTenant();
+        }
+
+        private void CreateDefaultTenant()
+        {
+            // Default tenant
+
+            var defaultTenant = _context.Tenants.IgnoreQueryFilters().FirstOrDefault(t => t.TenancyName == AbpTenantBase.DefaultTenantName);
+            if (defaultTenant == null)
+            {
+                defaultTenant = new Tenant(AbpTenantBase.DefaultTenantName, AbpTenantBase.DefaultTenantName);
+
+                var defaultEdition = _context.Editions.IgnoreQueryFilters().FirstOrDefault(e => e.Name == EditionManager.DefaultEditionName);
+                if (defaultEdition != null)
+                {
+                    defaultTenant.EditionId = defaultEdition.Id;
+                }
+
+                _context.Tenants.Add(defaultTenant);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
